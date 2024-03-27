b87cd58c2c2fafa986302e596d165b0c42031dc0
MS-Shahzaib-code <160013511+MS-Shahzaib-code@users.noreply.github.com>
2024-03-27 05:08:52 -0700

    created a number guessin game

---
+++ index.js
@@ -0,0 +1,13 @@
+#! /usr/bin/evn/ node
+import inquirer from "inquirer";
+const random = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    { message: "please guess a number between 1 to 10 ", name: "yourguessnumber", type: "number" }
+]);
+console.log(answer);
+if (answer.yourguessnumber === random) {
+    console.log("congtratulation you win the game");
+}
+else {
+    console.log("try again");
+}
